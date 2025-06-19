c44e4a4fb68f6ef675a169e867893118a2ce78b5
reaganclever <kasulereagan86@gmail.com>
2025-06-19 19:20:51 +0300

    Intital

---
+++ ReturnOnInvestiment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "TaskSixProject.h"
+
+double calculateTheReturnOnInvestiment(double investiment  , double profit){
+
+double ROI = (investiment/profit) * 100;
+printf("\n\n\e[1mNO.4\e[0m:  THE RETURN ON INVESTIMENT IS %.3lf\n" , ROI);
+
+return ROI;
+}
+
+
