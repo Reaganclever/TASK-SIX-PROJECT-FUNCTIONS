3a8ba631c76604dc13ea8c7b38aae43b6c6f358b
reaganclever <kasulereagan86@gmail.com>
2025-06-19 19:09:41 +0300

    Initial

---
+++ GrossProfit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "TaskSixProject.h"
+
+
+void grossProfit(){
+float salesVolume = 15000000 , COGS = 9000000;
+float grossProfit = salesVolume - COGS;
+printf("\e[1mNO.3\e[0m:  THE GROSS PROFIT IS %.3f" , grossProfit);
+}
