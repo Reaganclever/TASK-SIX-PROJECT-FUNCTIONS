b8e7c10c5efa2d688ba5c9a79bc03650143ca9ad
reaganclever <kasulereagan86@gmail.com>
2025-06-19 19:01:40 +0300

    TSPF-3 COMMIT#1 ON BRANCH#2

--- FinalSalaryAfterTax.c
+++ FinalSalaryAfterTax.c
@@ -2,7 +2,7 @@
 #include "TaskSixProject.h"
 
 void finalSalaryAfterTax(){
-    double originalSalary = 5000000 ,  taxRate = 0.15;
+    double originalSalary = 5235550 ,  taxRate = 0.17;
     double netSalary = originalSalary - (originalSalary * taxRate);
-    printf("\n\e[1mNO.2\e[0m:  THE FINAL SALARY AFTER TAX IS %.2lf\n\n" , netSalary);
+    printf("\n\e[1mNO.2\e[0m:  THE NEW FINAL SALARY AFTER TAX IS %.2lf\n\n" , netSalary);
 }
