bdd0c27dee067b2f405386b3151a0cb138fddb7d
reaganclever <kasulereagan86@gmail.com>
2025-06-19 19:55:32 +0300

    TSPF-6 COMMIT#1 ON BRANCH#5

--- MonthlyLoanPayment.c
+++ MonthlyLoanPayment.c
@@ -3,9 +3,9 @@
 #include <math.h>
 
 void calculateMonthlyLoanPayment() {
-    float P = 50002000;       // Loan amount
-    float annualR = 0.05;          // 5% annual interest
-    int years = 9;                    // Loan period in years
+    float P = 6000000;       // Loan amount
+    float annualR = 0.04;          // 5% annual interest
+    int years = 8;                    // Loan period in years
 
     float r = annualR/ 12; // Monthly rate
     int n = years * 12;      // Total number of payments
@@ -15,5 +15,5 @@ void calculateMonthlyLoanPayment() {
     float denominator = pow(1 + r , n) - 1;
     float monthlyPayment = numerator / denominator;
 
-    printf("\n\e[1mNO.5\e[0m: NEW MONTHLY LOAN PAYMENT: UGX %.2f\n", monthlyPayment);
+    printf("\n\e[1mNO.5\e[0m: NEWER MONTHLY LOAN PAYMENT: UGX %.2f\n", monthlyPayment);
 }
